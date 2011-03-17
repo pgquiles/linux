bbbd425b4d0f7ad914c735c825c83f3bd4b1dafd
Thomas Gleixner <tglx@linutronix.de>
2011-03-17 11:32:28 +0100

    mm: Fixup all fault handlers to check current->pagefault_disable
    Necessary for decoupling pagefault disable from preempt count.
    
    [pgquiles] Update patch for AmLogic kernel 3.10.87 + add arm64, arc
               and other architectures
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Pau Garcia i Quiles <pgquiles@elpauer.org>

--- arch/arc/mm/fault.c
+++ arch/arc/mm/fault.c
@@ -84,7 +84,7 @@ void do_page_fault(struct pt_regs *regs, int write, unsigned long address,
 	 * If we're in an interrupt or have no user
 	 * context, we must not take the fault..
 	 */
-	if (in_atomic() || !mm)
+	if (in_atomic() || !mm || current->pagefault_disabled)
 		goto no_context;
 
 	if (user_mode(regs))
